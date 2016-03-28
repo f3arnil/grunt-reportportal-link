1f44a94575cecac68d6d2ed09386a7736a44ce29
Alex Vasilyeu <f3arnil@gmail.com>
2016-03-28 10:43:16 +0300

    Fixes.

--- index.js
+++ index.js
@@ -16,7 +16,9 @@ module.exports = function (params, callback) {
         content: ".js-content"
     };
 
-    $(thisOptions.content).find('.b-docs__wrapper p').each(function (i, elem) {
+    $(thisOptions.content).find('.b-docs__wrapper').each(function (i, elem) {
+        if ($(elem).hasClass('docs-heading')) return;
+        if ($(elem).attr('id') === 'toc') return;
         $(elem).find('a').attr('target', '_blank');
     });
 
